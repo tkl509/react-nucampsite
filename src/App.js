65a958284e7a57f0d7268a53a1ebc3640f63f66a
Tami Lankford <tklankford@gmail.com>
2019-11-25 11:28:33 -0600

    Components part 1 - added and rendered campsites

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrnd } from 'reactstrap';
-//import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 class App extends Component {
   render() {
@@ -30,9 +10,10 @@ class App extends Component {
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrnd href="/">NuCamp</NavbarBrnd>
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
+        <Directory />
       </div>
     );
   }
