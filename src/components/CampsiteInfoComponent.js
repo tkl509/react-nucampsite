caf88e85bf58743f086dfa36d13e1672f9b9dc4e
Tami Lankford <tklankford@gmail.com>
2019-11-30 12:46:23 -0600

    added CampsiteInfo component

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+
+    renderCampsite(campsite) {
+        if (campsite) {
+            return (
+                <div className="col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        return <div />;
+    }
+
+    renderComments(comments) {
+        if(comments) {
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => {
+                        return(
+                            <div key={comment.id}>{comment.text}<p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p></div>);
+                    })}
+                </div>
+            );
+        }
+        return <div />
+    }
+
+    render() {
+
+        if(this.props.campsite) {
+            return(
+            <div className="row">
+                {this.renderCampsite(this.props.campsite)}
+                {this.renderComments(this.props.campsite.comments)}
+            </div>
+            );
+        }
+
+        return(
+            <div />
+        );
+    }
+
+}
+
+export default CampsiteInfo;
