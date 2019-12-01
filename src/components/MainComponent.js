f0349a91cf2a5e322290fa8f29df4c72c676d1fe
Tami Lankford <tklankford@gmail.com>
2019-12-01 17:17:54 -0600

    added React Routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+            campsites: CAMPSITES
         };
     }
 
-    onCampsiteSelect(campsiteID) {
-        this.setState({selectedCampsite: campsiteID});
-    }
-
     render() {
+
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
+
         return(
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteID => this.onCampsiteSelect(campsiteID)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} /> 
+                    <Redirect to='/home' />
+                </Switch> 
                 <Footer />
             </div>
         );
-    };
+    }
 
 }
 
