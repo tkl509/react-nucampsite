31a077ca6d881e107c7a9c6c17d0640316fa726f
Tami Lankford <tklankford@gmail.com>
2019-12-01 18:59:28 -0600

    added featured partners, promotions, campsite to single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -18,7 +25,11 @@ class Main extends Component {
 
         const HomePage = () => {
             return(
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         }
 
@@ -28,6 +39,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} /> 
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch> 
                 <Footer />
---
+++ src/shared/comments.js
@@ -0,0 +1,163 @@
+export const COMMENTS = 
+[
+    {
+        id: 0,
+        campsiteId: 0,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "0 Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        campsiteId: 0,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "0 Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        campsiteId: 0,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "0 McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        campsiteId: 0,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "0 Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        campsiteId: 0,
+        rating: 4, 
+        text: "The stars at night were magnificent!",
+        author: "0 Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 0,
+        campsiteId: 1,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "1 Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        campsiteId: 1,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "1 Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        campsiteId: 1,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "1 McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        campsiteId: 1,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "1 Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        campsiteId: 1,
+        rating: 4, 
+        text: "The stars at night were magnificent!",
+        author: "1 Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 0,
+        campsiteId: 2,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "2 Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        campsiteId: 2,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "2 Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        campsiteId: 2,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "2 McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        campsiteId: 2,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "2 Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        campsiteId: 2,
+        rating: 4, 
+        text: "The stars at night were magnificent!",
+        author: "2 Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 0,
+        campsiteId: 3,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "3 Tinus Lorvaldes",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        campsiteId: 3,
+        rating: 4, 
+        text: "The campground was beautiful, but the bugs could bite sometimes.",
+        author: "3 Brennen Ech",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        campsiteId: 3,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "3 McKenzie Sebastian",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        campsiteId: 3,
+        rating: 5, 
+        text: "A wonderful place to reconnect with nature.",
+        author: "3 Jordan Runn",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        campsiteId: 3,
+        rating: 4, 
+        text: "The stars at night were magnificent!",
+        author: "3 Ann Dabramov",
+        date: "2018-07-23T19:44Z"
+    }
+]
