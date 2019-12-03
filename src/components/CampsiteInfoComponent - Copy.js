2ebcbd4fcda5bd7c32f03747d429fde949dd3fd4
Tami Lankford <tklankford@gmail.com>
2019-12-02 21:27:53 -0600

    breadcrumbs added - end of week 3

--- src/components/CampsiteInfoComponent - Copy.js
+++ src/components/CampsiteInfoComponent - Copy.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 function RenderCampsite({campsite}) {
-    return (
-        <div className="col-md-5 m-1">
-            <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    );
+    if (campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+    return <div />;
 }
 
 function RenderComments({comments}) {
@@ -35,16 +38,6 @@ function CampsiteInfo(props) {
     if(props.campsite) {
         return(
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 function Contact(props) {
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                    </Breadcrumb>
                     <h2>Contact Us</h2>
                     <hr />
                 </div>
             </div>
-
+{/*            <div className="row">
+                <div className="col">
+                    <h2>Contact Us</h2>
+                    <hr />
+                </div>
+            </div>
+    */}
             <div className="row row-content align-items-center">
                 <div className="col-sm-4">
                     <h5>Our Address</h5>
