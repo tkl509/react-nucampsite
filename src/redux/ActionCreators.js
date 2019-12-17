f47a22e6f96693db20daea6aea1771c6500163d2
Tami Lankford <tklankford@gmail.com>
2019-12-17 13:00:16 -0600

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -6,15 +6,44 @@ export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
 
     return fetch(baseUrl + 'campsites')
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+        error => {
+            const errMes = new Error(error.message);
+            throw errMes;
+        }
+    )
     .then(response => response.json())
-    .then(campsites => dispatch(addCampsites(CAMPSITES)));
-    
+    .then(campsites => dispatch(addCampsites(CAMPSITES)))
+    .catch(error => dispatch(campsitesFailed(error.message)));
 };
 
 export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
+    .then(response => {
+        if(response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`)
+            error.response = response;
+            throw error;
+        }
+    },
+        error => {
+            var errMes = new Error(error.message);
+            throw errMes;
+        }
+    )
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = errMess => ({
@@ -31,8 +60,23 @@ export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
 
     return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`)
+            error.response = response;
+            throw error;
+        }
+    },
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        }
+    )
     .then(response => response.json())
-    .then(promotions => dispatch(addPromotions(promotions)));
+    .then(promotions => dispatch(addPromotions(promotions)))
+    .catch(error => dispatch(promotionsFailed(error.message)));
 }
 
 export const promotionsLoading = () => ({
